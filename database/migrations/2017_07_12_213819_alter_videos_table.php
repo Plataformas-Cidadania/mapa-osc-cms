6a426a59f1ba47eabf6db03b7ac265c611a3404b
root <root@evbsb1052.ipea.gov.br>
2017-07-12 17:41:10 -0300

    migrations

--- database/migrations/2017_07_12_213819_alter_videos_table.php
+++ database/migrations/2017_07_12_213819_alter_videos_table.php
@@ -13,7 +13,7 @@ class AlterVideosTable extends Migration
     public function up()
     {
         Schema::table('videos', function (Blueprint $table) {
-            $table->date('data')->default('0000-00-00');
+            $table->date('data')->nullable();
             $table->text('resumida')->default('');
             $table->text('descricao')->default('');
             $table->string('imagem')->default('');
